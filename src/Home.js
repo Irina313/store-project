0c6c5c67a68b17f340d84cf441d31f142e4cb423
Irina <irina1989_ru@ukr.net>
2025-09-10 10:39:23 -0500

    first commit

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,17 @@
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+
 
 
 function Store() {
+
+const handleAlert = () => {
+    Swal.fire("Get 15% off on your first order!");
+}
+   
     return <div className="main">
      <Link to="/shop"><button className="btn">Looking Collection</button></Link>  
+     <button className="discount" onClick={handleAlert}>Get 15% OFF</button>
     </div>
     }
     
